c4bafeadf010cfb2ed3daeab70db39ac1ad3566a
mozodev <dev@mozo.kr>
2014-08-15 00:07:49 +0900

    Applied to Nanum Gothic web font

--- js/_main.js
+++ js/_main.js
@@ -26,3 +26,19 @@ $(document).ready(function(){
 	// Target your .container, .wrapper, .post, etc.
 	$("#main").fitVids();
 });
+
+WebFontConfig = {
+custom: {
+    families: ['Nanum Gothic'],
+    urls: ['http://fonts.googleapis.com/earlyaccess/nanumgothic.css']
+}
+};
+(function() {
+var wf = document.createElement('script');
+wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+  '://ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js';
+wf.type = 'text/javascript';
+wf.async = 'true';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(wf, s);
+})(); 
