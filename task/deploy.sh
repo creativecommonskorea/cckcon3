f77a7950e2a5d0c88ad985715c778c8b2bd274fb
mozodev <mozodev@cckorea.org>
2014-08-18 11:21:49 +0900

    changed logo, footer styling

--- task/deploy.sh
+++ task/deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-jekyll build && rsync -avz --delete _site/ web01:/home/cckorea/www/2014con
+jekyll build --config=task/production.yml && rsync -avz --delete _site/ web01:/home/cckorea/www/2014con
