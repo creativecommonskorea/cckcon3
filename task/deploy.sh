ea178173243d656ee878f63f24bfc2b06fe90757
mozodev <mozodev@cckorea.org>
2014-10-13 11:40:40 +0900

    Checkout master for deployment

--- task/deploy.sh
+++ task/deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-jekyll build --config=task/production.yml && rsync -avz --delete _site/ web01:/home/cckorea/www/2014con
+git checkout master && jekyll build --config=task/production.yml && rsync -avz --delete _site/ web01:/home/cckorea/www/2014con
